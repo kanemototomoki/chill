92b99c3781ada5e7b346d6f75a6b5260372de6e2
kanemototomoki <kanemototomoki@gmail.com>
2024-10-16 22:00:58 +0900

    update

--- server.ts
+++ server.ts
@@ -2,3 +2,9 @@ import { type ServerBuild, createRequestHandler } from "react-router";
 import * as build from "./build/server";
 
 export const onRequest = createRequestHandler(build as unknown as ServerBuild);
+
+export default {
+  async fetch(request: Request) {
+    return await onRequest(request);
+  },
+};
